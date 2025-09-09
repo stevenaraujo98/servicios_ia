cc0a0eb2fdb5e1365974adcc45d1bcca376b3c12
Steven Araujo <steven.araujo@hotmail.com>
2025-09-08 22:59:27 -0500

    include_router objetivo

--- app/main.py
+++ app/main.py
@@ -6,11 +6,12 @@ from celery.result import AsyncResult
 from .projects.ods import ods_router
 from .projects.patente import patente_router
 from .projects.carrera import carrera_router
-from .projects.objetivo import calificate_objective, calificate_objectives_gen_esp
+from .projects.objetivo import objetivo_router
+from .projects.objetivos_gen_spe import calificate_objectives_gen_esp
 
 # --- Importaciones de tu proyecto ---
 from .validations import validate_min_length, validate_not_empty, clean_text
-from .modelsEntity import ItemContent, ItemContentObjectives, ItemModelContent, ItemModelContentObjectives, PredictionResponseClassificationObjective, FullEvaluationResponse, TaskCreationResponse, TaskStatusResponse
+from .modelsEntity import ItemContentObjectives, ItemModelContentObjectives, FullEvaluationResponse, TaskCreationResponse, TaskStatusResponse
 
 # --- Importaciones de Celery ---
 # Importamos la instancia de Celery y la tarea específica
@@ -54,64 +55,11 @@ app.include_router(
     tags=["Carrera"],
 )
 
-# Calificador Objetivo
-# @app.get("/predict/objetivo/")
-# def read_objetivo():
-#     objetivo = "Aumentar la satisfacción del cliente en un 15% para el tercer trimestre de 2025, implementando un nuevo sistema de soporte en línea y capacitando al equipo de atención al cliente."
-#     print(f"Objective text: {objetivo[:100]}...")
-
-#     model_name = "gemma3"
-#     approved, verbs, detail, suggestions, suggestion_options = calificate_objective(model_name, objetivo)
-
-#     print(f"Approved: {approved}")
-#     print(f"Verbs: {verbs}")
-#     print(f"Detail: {detail}")
-#     print(f"Suggestions: {suggestions}")
-#     print(f"Suggestion Options: {suggestion_options}")
-
-#     return {"model_name": model_name, "approved": approved, "verbs": verbs, "detail": detail, "suggestions": suggestions, "suggestion_options": suggestion_options}
-
-@app.post("/predict/objetivo/", response_model=PredictionResponseClassificationObjective)
-def predict_objetivo(item: ItemModelContent, q: Union[str, None] = None):
-    if q:
-        print(f"Query parameter q: {q}")
-
-    model_name = item.model_name.strip()
-    validate_not_empty(model_name)
-
-    objetivo = clean_text(item.content)
-    # validate objetivo min limit_min
-    validate_min_length(objetivo, min_length=10)
-
-    approved, verbs, detail, suggestions, suggestion_options = calificate_objective(model_name, objetivo)
-
-    print(f"Approved: {approved}")
-    print(f"Verbs: {verbs}")
-    print(f"Detail: {detail}")
-    print(f"Suggestions: {suggestions}")
-    print(f"Suggestion Options: {suggestion_options}")
-    return {"approved": approved, "verbs": verbs, "detail": detail, "suggestions": suggestions, "suggestion_options": suggestion_options}
-
-@app.post("/predict/objetivo/{model_name}", response_model=PredictionResponseClassificationObjective)
-def predict_objetivo(model_name: str, item: ItemContent, q: Union[str, None] = None):
-    if q:
-        print(f"Query parameter q: {q}")
-    validate_not_empty(model_name)
-
-    objetivo = clean_text(item.content)
-    # validate objetivo min limit_min
-    validate_min_length(objetivo, min_length=10)
-
-    approved, verbs, detail, suggestions, suggestion_options = calificate_objective(model_name, objetivo)
-
-    print(f"Approved: {approved}")
-    print(f"Verbs: {verbs}")
-    print(f"Detail: {detail}")
-    print(f"Suggestions: {suggestions}")
-    print(f"Suggestion Options: {suggestion_options}")
-    return {"approved": approved, "verbs": verbs, "detail": detail, "suggestions": suggestions, "suggestion_options": suggestion_options}
-
-
+app.include_router(
+    objetivo_router,
+    prefix="/predict/objetivo",
+    tags=["Objetivo"],
+)
 
 # # Calificador Objetivos
 # @app.get("/predict/objetivos/")
