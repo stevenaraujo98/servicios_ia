5fd45b1ea6b390cd7c0ed3f9653651e4647b13e2
Steven Araujo <steven.araujo@hotmail.com>
2025-09-08 22:36:40 -0500

    include_router patente

--- app/main.py
+++ app/main.py
@@ -4,13 +4,13 @@ from fastapi.middleware.cors import CORSMiddleware
 from celery.result import AsyncResult
 
 from .projects.ods import predict_ods_text
-from .projects.patente import predict_patent_text
+from .projects.patente import patente_router
 from .projects.carrera import predict_carrera_text
 from .projects.objetivo import calificate_objective, calificate_objectives_gen_esp
 
 # --- Importaciones de tu proyecto ---
 from .validations import validate_min_length, validate_not_empty, clean_text
-from .modelsEntity import ItemContent, ItemContentObjectives, ItemModelContent, ItemModelContentObjectives, PredictionResponse, PredictionResponseODS, PredictionResponseCareer, PredictionResponseClassificationObjective, FullEvaluationResponse, TaskCreationResponse, TaskStatusResponse
+from .modelsEntity import ItemContent, ItemContentObjectives, ItemModelContent, ItemModelContentObjectives, PredictionResponseODS, PredictionResponseCareer, PredictionResponseClassificationObjective, FullEvaluationResponse, TaskCreationResponse, TaskStatusResponse
 from .models.ModelLoader import ModelLoader
 
 # --- Importaciones de Celery ---
@@ -35,19 +35,16 @@ app.add_middleware(
 
 # Inicializar cargador de modelos
 loader_ods = ModelLoader()
-loader_patente = ModelLoader(tipo='patente')
 loader_carrera = ModelLoader(tipo='carrera')
 
 print("Cargando modelos de transformadores...")
 loader_ods.load_transformer_model("distilbert_10e_24b_0") # Cashear el modelo para evitar recargas innecesarias
-# loader_patente.load_transformer_model("distilbert_20250710_0233") # Cashear el modelo para evitar recargas innecesarias
 # 1.2.2 de kaggle
 # loader_carrera.load_transformer_model("bert_20250806_234119") # Cashear el modelo para evitar recargas innecesarias
 print("Finalizó carga de modelos de transformadores...")
 
 print("Cargando modelos tradicionales...")
 # loader_ods.load_traditional_model("Logistic_Regression_20250611_165546") # Cashear el modelo para evitar recargas innecesarias
-loader_patente.load_traditional_model("Random_Forest_20250813_144340") # Cashear el modelo para evitar recargas innecesarias
 loader_carrera.load_traditional_model("Random_Forest_20250808_161322") # Cashear el modelo para evitar recargas innecesarias
 print("Finalizó carga de modelos tradicionales...")
 
@@ -56,60 +53,11 @@ def read_root():
     return {"Hello": "IA"}
 
 
-
-# PATENTE
-# @app.get("/predict/patente/")
-# def read_project():
-#     sample_text = "METHOD FOR MANAGING INDOOR BEACON-BASED COMMUNICATION A method for content distribution in an indoor space and surrounding area covered by beacon signals. The method includes setting general data of the content, setting at least one location in the indoor space, including one or a combination of active/inactive locations and other relevant information available in a system, assigning beacons that trigger the content in the indoor space, receiving by the system, from a portable device via a first communication channel, beacon information that is received from a beacon by the portable device via a second communication channel, the first communication channel being different from the second communication channel, setting a singular event or plural events which initiates the content in the indoor space, setting a condition formula for the content which must be fulfilled to qualify for the content, and setting singular or plural results of the content that are provided when the condition formula is fulfilled."
-#     print(f"Sample text: {sample_text[:100]}...")
-
-#     model_name = "Random_Forest_20250708_144028"
-#     prediction, probability, predictions, probabilities = predict_patent_text(loader_patente, model_name, sample_text)
-
-#     print(f"Prediction: {prediction} with probability: {probability}")
-#     print(f"Predictions: {predictions}")
-#     print(f"Probabilities: {probabilities}")
-
-#     return {"model_name": model_name, "prediction": prediction, "probability": probability, "predictions": predictions, "probabilities": probabilities}
-
-@app.post("/predict/patente/", response_model=PredictionResponse)
-def predict_project(item: ItemModelContent, q: Union[str, None] = None):
-    if q:
-        print(f"Query parameter q: {q}")
-
-    model_name = item.model_name.strip()
-    validate_not_empty(model_name)
-
-    sample_text = clean_text(item.content)
-    # validate sample_text min limit_min
-    validate_min_length(sample_text)
-
-    prediction, probability, predictions, probabilities = predict_patent_text(loader_patente, model_name, sample_text)
-    probabilities = probabilities[0] if len(probabilities) > 0 else None  # Asegurar que las probabilidades sean una lista
-    print(f"Prediction: {prediction} with probability: {probability}")
-    print(f"Predictions: {predictions}")
-    print(f"Probabilities: {probabilities}")
-    return {"prediction": prediction, "probability": probability, "predictions": predictions, "probabilities": probabilities}
-
-@app.post("/predict/patente/{model_name}", response_model=PredictionResponse)
-def predict_project(model_name: str, item: ItemContent, q: Union[str, None] = None):
-    if q:
-        print(f"Query parameter q: {q}")
-
-    validate_not_empty(model_name)
-
-    sample_text = clean_text(item.content)
-    # validate sample_text min limit_min
-    validate_min_length(sample_text)
-
-    prediction, probability, predictions, probabilities = predict_patent_text(loader_patente, model_name, sample_text)
-    probabilities = probabilities[0] if len(probabilities) > 0 else None  # Asegurar que las probabilidades sean una lista
-    print(f"Prediction: {prediction} with probability: {probability}")
-    print(f"Predictions: {predictions}")
-    print(f"Probabilities: {probabilities}")
-    return {"prediction": prediction, "probability": probability, "predictions": predictions, "probabilities": probabilities}
-
-
+app.include_router(
+    patente_router,
+    prefix="/predict/patente",
+    tags=["Patente"],
+)
 
 # ODS
 # @app.get("/predict/ods/")
--- app/projects/patente.py
+++ app/projects/patente.py
@@ -1,7 +1,19 @@
-import numpy as np
 import os
-from ..models.ModelLoader import crear_corpus, detect_language_and_translate_es_en
-from fastapi import HTTPException
+import numpy as np
+from typing import Union
+from fastapi import HTTPException, APIRouter
+from ..modelsEntity import ItemContent, ItemModelContent, PredictionResponse
+from ..validations import validate_min_length, validate_not_empty, clean_text
+from ..models.ModelLoader import ModelLoader, crear_corpus, detect_language_and_translate_es_en
+
+loader_patente = ModelLoader(tipo='patente')
+
+# print("Cargando modelos de patente tradicionales...")
+# loader_patente.load_transformer_model("distilbert_20250710_0233") # Cashear el modelo para evitar recargas innecesarias
+print("Cargando modelos de patente tradicionales...")
+loader_patente.load_traditional_model("Random_Forest_20250813_144340") # Cashear el modelo para evitar recargas innecesarias
+
+patente_router = APIRouter()
 
 def predict_patent_text(model_loader, model_folder, text, model_type='auto'):
     """Predice etiquetas para textos individuales"""
@@ -52,3 +64,54 @@ def predict_patent_text(model_loader, model_folder, text, model_type='auto'):
         print(f"   Predicción: {pred}{prob_str}")
 
     return int(prediction), round(float(probability), 2) if probability is not None else None, predictions, probabilities
+
+# @app.get("/predict/patente/")
+# def read_project():
+#     sample_text = "METHOD FOR MANAGING INDOOR BEACON-BASED COMMUNICATION A method for content distribution in an indoor space and surrounding area covered by beacon signals. The method includes setting general data of the content, setting at least one location in the indoor space, including one or a combination of active/inactive locations and other relevant information available in a system, assigning beacons that trigger the content in the indoor space, receiving by the system, from a portable device via a first communication channel, beacon information that is received from a beacon by the portable device via a second communication channel, the first communication channel being different from the second communication channel, setting a singular event or plural events which initiates the content in the indoor space, setting a condition formula for the content which must be fulfilled to qualify for the content, and setting singular or plural results of the content that are provided when the condition formula is fulfilled."
+#     print(f"Sample text: {sample_text[:100]}...")
+
+#     model_name = "Random_Forest_20250708_144028"
+#     prediction, probability, predictions, probabilities = predict_patent_text(loader_patente, model_name, sample_text)
+
+#     print(f"Prediction: {prediction} with probability: {probability}")
+#     print(f"Predictions: {predictions}")
+#     print(f"Probabilities: {probabilities}")
+
+#     return {"model_name": model_name, "prediction": prediction, "probability": probability, "predictions": predictions, "probabilities": probabilities}
+
+@patente_router.post("/", response_model=PredictionResponse)
+def predict_project(item: ItemModelContent, q: Union[str, None] = None):
+    if q:
+        print(f"Query parameter q: {q}")
+
+    model_name = item.model_name.strip()
+    validate_not_empty(model_name)
+
+    sample_text = clean_text(item.content)
+    # validate sample_text min limit_min
+    validate_min_length(sample_text)
+
+    prediction, probability, predictions, probabilities = predict_patent_text(loader_patente, model_name, sample_text)
+    probabilities = probabilities[0] if len(probabilities) > 0 else None  # Asegurar que las probabilidades sean una lista
+    print(f"Prediction: {prediction} with probability: {probability}")
+    print(f"Predictions: {predictions}")
+    print(f"Probabilities: {probabilities}")
+    return {"prediction": prediction, "probability": probability, "predictions": predictions, "probabilities": probabilities}
+
+@patente_router.post("/{model_name}", response_model=PredictionResponse)
+def predict_project(model_name: str, item: ItemContent, q: Union[str, None] = None):
+    if q:
+        print(f"Query parameter q: {q}")
+
+    validate_not_empty(model_name)
+
+    sample_text = clean_text(item.content)
+    # validate sample_text min limit_min
+    validate_min_length(sample_text)
+
+    prediction, probability, predictions, probabilities = predict_patent_text(loader_patente, model_name, sample_text)
+    probabilities = probabilities[0] if len(probabilities) > 0 else None  # Asegurar que las probabilidades sean una lista
+    print(f"Prediction: {prediction} with probability: {probability}")
+    print(f"Predictions: {predictions}")
+    print(f"Probabilities: {probabilities}")
+    return {"prediction": prediction, "probability": probability, "predictions": predictions, "probabilities": probabilities}
